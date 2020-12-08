6f4ea33073ac2bbf9c54d6d0aed8d5c21e6c607f
Simon Bredeche <simon.bredeche@u-psud.fr>
2020-12-08 23:21:39 +0100

    addedCollision

--- src/pokonline/client/vues/PlayerView.java
+++ src/pokonline/client/vues/PlayerView.java
@@ -11,31 +11,31 @@ public class PlayerView {
 
    public static void render(PlayerModeles player, Graphics g) throws SlickException {
 	   g.setColor(Color.red);
-	   g.drawString(player.getName(), player.getX(), player.getY()-20);
+	   g.drawString(player.getName(), player.getX(), player.getY()-100);
 	   switch(player.getDirection()) {
 	   case("down"):
 		   if(player.isMoving())
-			   g.drawAnimation(player.getAnimation()[4],player.getX(),player.getY());
+			   g.drawAnimation(player.getAnimation()[4],player.getX()-(player.getAnimation()[0].getWidth()/2),player.getY()-(player.getAnimation()[0].getHeight()/2));
 		   else
-			   g.drawAnimation(player.getAnimation()[0],player.getX(),player.getY());
+			   g.drawAnimation(player.getAnimation()[0],player.getX()-(player.getAnimation()[0].getWidth()/2),player.getY()-(player.getAnimation()[0].getHeight()/2));
 		   break;
 	   case("left"):
 		   if(player.isMoving())
-			   g.drawAnimation(player.getAnimation()[5],player.getX(),player.getY());
+			   g.drawAnimation(player.getAnimation()[5],player.getX()-(player.getAnimation()[0].getWidth()/2),player.getY()-(player.getAnimation()[0].getHeight()/2));
 		   else
-			   g.drawAnimation(player.getAnimation()[1],player.getX(),player.getY());
+			   g.drawAnimation(player.getAnimation()[1],player.getX()-(player.getAnimation()[0].getWidth()/2),player.getY()-(player.getAnimation()[0].getHeight()/2));
 		   break;
 	   case("right"):
 		   if(player.isMoving())
-			   g.drawAnimation(player.getAnimation()[6],player.getX(),player.getY());
+			   g.drawAnimation(player.getAnimation()[6],player.getX()-(player.getAnimation()[0].getWidth()/2),player.getY()-(player.getAnimation()[0].getHeight()/2));
 		   else
-			   g.drawAnimation(player.getAnimation()[2],player.getX(),player.getY());
+			   g.drawAnimation(player.getAnimation()[2],player.getX()-(player.getAnimation()[0].getWidth()/2),player.getY()-(player.getAnimation()[0].getHeight()/2));
 		   break;
 	   case("up"):
 		   if(player.isMoving())
-			   g.drawAnimation(player.getAnimation()[7],player.getX(),player.getY());
+			   g.drawAnimation(player.getAnimation()[7],player.getX()-(player.getAnimation()[0].getWidth()/2),player.getY()-(player.getAnimation()[0].getHeight()/2));
 		   else
-			   g.drawAnimation(player.getAnimation()[3],player.getX(),player.getY());
+			   g.drawAnimation(player.getAnimation()[3],player.getX()-(player.getAnimation()[0].getWidth()/2),player.getY()-(player.getAnimation()[0].getHeight()/2));
 		   break;
 	   }
 
